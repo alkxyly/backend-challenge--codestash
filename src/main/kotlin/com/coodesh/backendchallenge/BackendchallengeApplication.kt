4d1e505f45fd706d2d26be27aa1579c36a15200a
alkxyly <alkxyly@hotmail.com>
2022-03-05 18:24:26 -0300

    first commit

---
+++ src/main/kotlin/com/coodesh/backendchallenge/BackendchallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.coodesh.backendchallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendchallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendchallengeApplication>(*args)
+}
